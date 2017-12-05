cca7e9a1e8259ae951214792c64e65d75700911f
Lucas Wiener <lucas@wiener.se>
2017-12-05 12:48:07 +0100

    websocket a working POC

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [http-kit "2.1.18"]
                  [compojure "1.5.1"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.cognitect/transit-clj "0.8.300"]
+                 [ysera "1.0.3"]]
   :main ^:skip-aot chess.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chess/server.clj
+++ src/chess/server.clj
@@ -4,42 +4,111 @@
     [org.httpkit.server :refer :all]
     [compojure.core :refer :all]
     [clojure.data.json :as json]
-    [chess.mapper :refer :all]))
+    [chess.mapper :refer :all]
+    [cognitect.transit :as transit]
+    [ysera.error :refer [error]]))
+(import [java.io ByteArrayInputStream ByteArrayOutputStream])
+
+(defonce websocket-atom (atom {:channels #{}}))
+
+(defn get-client-channels []
+  (:channels (deref websocket-atom)))
+
+(defn connect! [channel]
+  (println "client connected")
+  (swap! websocket-atom update :channels conj channel))
+
+(defn disconnect! [channel status]
+  (println "client disconnected:" status)
+  (swap! websocket-atom update :channels (fn [channels] (remove #{channel} channels))))
+
+(defn transit-write [x]
+  (let [baos (ByteArrayOutputStream.)
+        w (transit/writer baos :json)
+        _ (transit/write w x)
+        ret (.toString baos)]
+    (.reset baos)
+    ret))
+
+(defn notify-clients [msg]
+  (doseq [channel (get-client-channels)]
+    (send! channel (transit-write msg))))
+
+(add-watch chess.games/game-atom
+           :websocket-client-notifier
+           (fn [_ _ _ _]
+             (println "something happened. notifying clients.")
+             (notify-clients {:data (game->view-game (deref chess.games/game-atom))})))
 
 ;; Routes
 
-(defn game-response [view-game]
-  {:status  200
-   :headers {"Content-Type"                 "text/json; charset=utf-8"
-             "Access-Control-Allow-Origin"  "*"
-             "Access-Control-Allow-Methods" "*"}
-   :body    (json/write-str view-game)})
+;(defn game-response [view-game]
+;  {:status  200
+;   :headers {"Content-Type"                 "text/json; charset=utf-8"
+;             "Access-Control-Allow-Origin"  "*"
+;             "Access-Control-Allow-Methods" "*"}
+;   :body    (json/write-str view-game)})
 
-(defroutes
-  chess
-  (POST "/createGame" []
-    (time (game-response (game->view-game (create-game!)))))
-  (POST "/move" {body :body}
-    (let [params (json/read-json (slurp body))
-          from-position (:from-position params)
-          to-position (:to-position params)
-          player-id (keyword (:player-id params))]
-      (time (game-response (game->view-game (move! player-id from-position to-position))))))
-  ;(POST "/castle" {body :body}
-  ;  (let [params (json/read-json (slurp body))
-  ;        from-position (:from-position params)
-  ;        to-position (:to-position params)
-  ;        player-id (keyword (:player-id params))]
-  ;    (time (game-response (game->view-game (castle! player-id from-position to-position))))))
-  (POST "/undo" {body :body}
-    (let [params (json/read-json (slurp body))
-          player-id (keyword (:player-id params))]
-      (time (game-response (game->view-game (undo! player-id))))))
-  (POST "/redo" {body :body}
-    (let [params (json/read-json (slurp body))
-          player-id (keyword (:player-id params))]
-      (time (game-response (game->view-game (redo! player-id)))))))
+;(defroutes
+;  chess
+;  (POST "/createGame" []
+;    (time (game-response (game->view-game (create-game!)))))
+;  (POST "/move" {body :body}
+;    (let [params (json/read-json (slurp body))
+;          from-position (:from-position params)
+;          to-position (:to-position params)
+;          player-id (keyword (:player-id params))]
+;      (time (game-response (game->view-game (move! player-id from-position to-position))))))
+;  ;(POST "/castle" {body :body}
+;  ;  (let [params (json/read-json (slurp body))
+;  ;        from-position (:from-position params)
+;  ;        to-position (:to-position params)
+;  ;        player-id (keyword (:player-id params))]
+;  ;    (time (game-response (game->view-game (castle! player-id from-position to-position))))))
+;  (POST "/undo" {body :body}
+;    (let [params (json/read-json (slurp body))
+;          player-id (keyword (:player-id params))]
+;      (time (game-response (game->view-game (undo! player-id))))))
+;  (POST "/redo" {body :body}
+;    (let [params (json/read-json (slurp body))
+;          player-id (keyword (:player-id params))]
+;      (time (game-response (game->view-game (redo! player-id)))))))
 
+(defn handle-client-action
+  [{action :action data :data}]
+  (condp = action
+    "create-game"
+    (create-game!)
+
+    "move-piece"
+    (let [from-position (:from-position data)
+          to-position (:to-position data)
+          player-id (:player-id data)]
+      (move! player-id from-position to-position))
+
+    "undo"
+    (let [player-id (:player-id data)]
+      (undo! player-id))
+
+    "redo"
+    (let [player-id (:player-id data)]
+      (redo! player-id))
+
+    (error "Unknown action:" action)))
+
+(defn chess-websocket
+  [request]
+  (with-channel request channel
+                (connect! channel)
+                (on-close channel (fn [status] (disconnect! channel status)))
+                (on-receive channel (fn [data]
+                                      (println data)
+                                      (-> data
+                                          (.getBytes "UTF-8")
+                                          (ByteArrayInputStream.)
+                                          (transit/reader :json)
+                                          (transit/read)
+                                          (handle-client-action))))))
 
 ;; Starting & Stopping
 
@@ -52,7 +121,8 @@
     (reset! server nil)))
 
 (defn start! []
-  (reset! server (run-server chess {:port 8001})))
+  ;; (reset! server (run-server chess {:port 8001}))
+  (run-server chess-websocket {:port 9876}))
 
 (defn restart! []
   (stop!)
